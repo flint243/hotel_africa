8b7d23f1c9dc7edc11989cc7b69e175b045d0379
flint243 <243mediaprod@gmail.com>
2024-04-14 01:16:18 +0200

    rectif des sliders header et des avis clients, responsive presque terminé

--- js/slideClassAvis.js
+++ js/slideClassAvis.js
@@ -6,11 +6,11 @@ class SlideShowAvis{
             'images/visage/femme1.jpg',
             'images/visage/homme1.jpg',
             'images/visage/femme2.jpg',
-            'images/visage/homme2.jpg',
+            'images/visage/homme3.jpg',
             'images/visage/homme4.jpg'
           ];
           this.textImg = [
-            '</h1><p>Nous sommes ravis de vous accueillir dans ce cadre somptueux</p> Profitez des délices gastronomiques tropicaux ainsi que des saveurs épicées.',
+            'Bienvenue chez <h1 id="deliceTropic" style="color:orange">Délice Tropical</h1><p>Nous sommes ravis de vous accueillir dans ce cadre somptueux</p> Profitez des délices gastronomiques tropicaux ainsi que des saveurs épicées.',
            
           '<h1></h1></h1>Consultez nos menus et voyagez au bout du monde<br> Nos cartes vous embarque dans une aventure gustative sans pareil.', 
          
@@ -18,7 +18,7 @@ class SlideShowAvis{
          
           'Après votre réservation vous avez tout de même la possibilité de vous désister à votre convenance 48h avant.', 
          
-          'vous souhaites une bonne ballade culinaire !'
+          '<h1 id="deliceTropic" style="color:orange">Délice Tropical</h1> vous souhaites une bonne ballade culinaire !'
         ];
           
           this.speed = 3000; //vitesse de defilement en milliseconds
