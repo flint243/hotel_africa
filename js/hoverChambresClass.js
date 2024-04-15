52930c3e1990db2826b74735e187e0f592f6f102
flint243 <243mediaprod@gmail.com>
2024-04-15 22:18:35 +0200

    rectif section hover index.html

--- js/hoverChambresClass.js
+++ js/hoverChambresClass.js
@@ -1,92 +1,144 @@
-class hoverChambres{
+class HoverChambres {
+    constructor() {
+        this.init();
+    }
 
-    constructor(){
+    init() {
         $('.chambre1').mouseover(this.changeColor1.bind(this));
-        $('.chambre1').mouseout(this.colorBack1.bind(this));
+        $('.chambre1').mouseout(this.hideColor1.bind(this));
 
         $('.chambre2').mouseover(this.changeColor2.bind(this));
-        $('.chambre2').mouseout(this.colorBack2.bind(this));
+        $('.chambre2').mouseout(this.hideColor2.bind(this));
 
         $('.chambre3').mouseover(this.changeColor3.bind(this));
-        $('.chambre3').mouseout(this.colorBack3.bind(this));
+        $('.chambre3').mouseout(this.hideColor3.bind(this));
 
         $('.chambre4').mouseover(this.changeColor4.bind(this));
-        $('.chambre4').mouseout(this.colorBack4.bind(this));
+        $('.chambre4').mouseout(this.hideColor4.bind(this));
 
         $('.chambre5').mouseover(this.changeColor5.bind(this));
-        $('.chambre5').mouseout(this.colorBack5.bind(this));
+        $('.chambre5').mouseout(this.hideColor5.bind(this));
+    }
 
-    } 
+    changeColor1() {
+        // Check if the background color is the default color (none or transparent)
+        if ($('.chambre1 .half').css('backgroundColor') === 'rgba(0, 0, 0, 0)' || $('.chambre1 .half').css('backgroundColor') === 'transparent') {
+            this.showColor1();
+        } else {
+            this.hideColor1();
+        }
+    }
 
-    changeColor1(){
-		$('.chambre1 .half').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.chambre1').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.cadreIn').css({'backgroundColor': 'white'});
-        $('.et1').css({'color': 'white'});
-	}
+    showColor1() {
+        $('.chambre1 .half').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.chambre1').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.cadreIn').css('backgroundColor', 'white');
+        $('.et1').css('color', 'white');
+    }
 
-	colorBack1(){
-		$('.chambre1 .half').css({'backgroundColor': 'none'});
-        $('.chambre1').css({'backgroundColor': 'none'});
-        $('.cadreIn').css({'backgroundColor': 'none'});
-        $('.et1').css({'backgroundColor': 'none'});
-	}
+    hideColor1() {
+        $('.chambre1 .half').css('backgroundColor', 'transparent');
+        $('.chambre1').css('backgroundColor', 'transparent');
+        $('.cadreIn').css('backgroundColor', 'transparent');
+        $('.et1').css('color', 'black'); // Change the color back to default or any other desired color
+    }
 
-    changeColor2(){
-		$('.chambre2 .half').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.chambre2').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.cadreIn').css({'backgroundColor': 'white'});
-        $('.et2').css({'color': 'white'});
-	}
+    changeColor2() {
+        // Check if the background color is the default color (none or transparent)
+        if ($('.chambre2 .half').css('backgroundColor') === 'rgba(0, 0, 0, 0)' || $('.chambre2 .half').css('backgroundColor') === 'transparent') {
+            this.showColor2();
+        } else {
+            this.hideColor2();
+        }
+    }
 
-	colorBack2(){
-		$('.chambre2 .half').css({'backgroundColor': 'none'});
-        $('.chambre2').css({'backgroundColor': 'none'});
-        $('.cadreIn').css({'backgroundColor': 'none'});
-        $('.et2').css({'backgroundColor': 'none'});
-	}
+    showColor2() {
+        $('.chambre2 .half').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.chambre2').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.cadreIn').css('backgroundColor', 'white');
+        $('.et2').css('color', 'white');
+    }
 
-    changeColor3(){
-		$('.chambre3 .half').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.chambre3').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.cadreIn').css({'backgroundColor': 'white'});
-        $('.et3').css({'color': 'white'});
-	}
+    hideColor2() {
+        $('.chambre2 .half').css('backgroundColor', 'transparent');
+        $('.chambre2').css('backgroundColor', 'transparent');
+        $('.cadreIn').css('backgroundColor', 'transparent');
+        $('.et2').css('color', 'black'); // Change the color back to default or any other desired color
+    }
 
-	colorBack3(){
-		$('.chambre3 .half').css({'backgroundColor': 'none'});
-        $('.chambre3').css({'backgroundColor': 'none'});
-        $('.cadreIn').css({'backgroundColor': 'none'});
-        $('.et3').css({'backgroundColor': 'none'});
-	}
 
-    changeColor4(){
-		$('.chambre4 .half').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.chambre4').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.cadreIn').css({'backgroundColor': 'white'});
-        $('.et4').css({'color': 'white'});
-	}
+    changeColor3() {
+        // Check if the background color is the default color (none or transparent)
+        if ($('.chambre3 .half').css('backgroundColor') === 'rgba(0, 0, 0, 0)' || $('.chambre3 .half').css('backgroundColor') === 'transparent') {
+            this.showColor3();
+        } else {
+            this.hideColor3();
+        }
+    }
 
-	colorBack4(){
-		$('.chambre4 .half').css({'backgroundColor': 'none'});
-        $('.chambre4').css({'backgroundColor': 'none'});
-        $('.cadreIn').css({'backgroundColor': 'none'});
-        $('.et4').css({'backgroundColor': 'none'});
-	}
+    showColor3() {
+        $('.chambre3 .half').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.chambre3').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.cadreIn').css('backgroundColor', 'white');
+        $('.et3').css('color', 'white');
+    }
 
-    changeColor5(){
-		$('.chambre5 .half').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.chambre5').css({'backgroundColor': 'rgb(218, 190, 9'});
-        $('.cadreIn').css({'backgroundColor': 'white'});
-        $('.et5').css({'color': 'white'});
-	}
+    hideColor3() {
+        $('.chambre3 .half').css('backgroundColor', 'transparent');
+        $('.chambre3').css('backgroundColor', 'transparent');
+        $('.cadreIn').css('backgroundColor', 'transparent');
+        $('.et3').css('color', 'black'); // Change the color back to default or any other desired color
+    }
 
-	colorBack5(){
-		$('.chambre5 .half').css({'backgroundColor': 'none'});
-        $('.chambre5').css({'backgroundColor': 'none'});
-        $('.cadreIn').css({'backgroundColor': 'none'});
-        $('.et5').css({'backgroundColor': 'none'});
-	}
 
+    changeColor4() {
+        // Check if the background color is the default color (none or transparent)
+        if ($('.chambre4 .half').css('backgroundColor') === 'rgba(0, 0, 0, 0)' || $('.chambre4 .half').css('backgroundColor') === 'transparent') {
+            this.showColor4();
+        } else {
+            this.hideColor4();
+        }
+    }
+
+    showColor4() {
+        $('.chambre4 .half').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.chambre4').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.cadreIn').css('backgroundColor', 'white');
+        $('.et4').css('color', 'white');
+    }
+
+    hideColor4() {
+        $('.chambre4 .half').css('backgroundColor', 'transparent');
+        $('.chambre4').css('backgroundColor', 'transparent');
+        $('.cadreIn').css('backgroundColor', 'transparent');
+        $('.et4').css('color', 'black'); // Change the color back to default or any other desired color
+    }
+
+
+    changeColor5() {
+        // Check if the background color is the default color (none or transparent)
+        if ($('.chambre5 .half').css('backgroundColor') === 'rgba(0, 0, 0, 0)' || $('.chambre5 .half').css('backgroundColor') === 'transparent') {
+            this.showColor5();
+        } else {
+            this.hideColor5();
+        }
+    }
+
+    showColor5() {
+        $('.chambre5 .half').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.chambre5').css('backgroundColor', 'rgb(218, 190, 9)');
+        $('.cadreIn').css('backgroundColor', 'white');
+        $('.et5').css('color', 'white');
+    }
+
+    hideColor5() {
+        $('.chambre5 .half').css('backgroundColor', 'transparent');
+        $('.chambre5').css('backgroundColor', 'transparent');
+        $('.cadreIn').css('backgroundColor', 'transparent');
+        $('.et5').css('color', 'black'); // Change the color back to default or any other desired color
+    }
 }
-hoverChambres =  new hoverChambres();
+
+$(document).ready(function() {
+    new HoverChambres();
+});
